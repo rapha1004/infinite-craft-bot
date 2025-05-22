f18c5b8d967497724a7258a6b78e3bef38fa8145
rapha1004 <106275964+rapha1004@users.noreply.github.com>
2025-05-22 15:01:23 +0200

    ..

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const middleW = window.innerWidth / 2;
 const middleH = window.innerHeight / 2;
 
+let craftList = [];
+let lastItem = document
+  .querySelector(".items-inner")
+  .lastChild.children[0].getAttribute("data-item-text");
 
 function simulateMouseEvent(element, eventType, clientX, clientY) {
   element.dispatchEvent(
@@ -14,7 +18,6 @@ function simulateMouseEvent(element, eventType, clientX, clientY) {
   );
 }
 
-
 function getAllElement() {
   const items = document.querySelectorAll(".item");
   const list = Array.from(items);
@@ -23,32 +26,56 @@ function getAllElement() {
 }
 
 async function controller() {
-  
+  console.log(craftList);
+
   const items = getAllElement();
 
   for (let index = 0; index < items.length; index++) {
-    
     for (let jndex = index + 1; jndex < items.length; jndex++) {
-      await wait(100)
+      await wait(100);
+      lastItem = document
+        .querySelector(".items-inner")
+        .lastChild.children[0].getAttribute("data-item-text");
+
       dragAndDrop(items[index], items[jndex]);
-      await wait(300)
-       erase();
+      await wait(300);
+      erase();
     }
   }
   console.log("je remprends les item la !");
-  controller()
+  controller();
 }
+
+function addToTable(first, second) {
+  const child = document
+    .querySelector(".items-inner")
+    .lastChild.children[0].getAttribute("data-item-text");
+  // TODO: le psuh dans le tableau de craft ne fonctionne pas
+
+  
+  
+  if (lastItem != child) {
+    console.log(first + " + " + second + " = " + child);
+    craftList.push({
+      item1: first.getAttribute("data-item-text"),
+      item2: second.getAttribute("data-item-text"),
+      result: child,
+    });
+  }
+  craftList.push({
+    item1: first.getAttribute("data-item-text"),
+    item2: second.getAttribute("data-item-text"),
+    result: none,
+  });
+
+}
+
+
 
 
 function dragAndDrop(first, second) {
   const firstRect = first.getBoundingClientRect();
   const secondRect = second.getBoundingClientRect();
-
-  console.log(
-    first.getAttribute("data-item-text") +
-      " + " +
-      second.getAttribute("data-item-text")
-  );
 
   //first
   simulateMouseEvent(first, "mousedown", firstRect.left + 5, firstRect.top + 5);
@@ -56,7 +83,7 @@ function dragAndDrop(first, second) {
   simulateMouseEvent(document, "mouseup", middleW, middleH);
 
   //second
-  setTimeout(() => {
+  setTimeout(async () => {
     simulateMouseEvent(
       second,
       "mousedown",
@@ -65,9 +92,15 @@ function dragAndDrop(first, second) {
     );
     simulateMouseEvent(document, "mousemove", middleW, middleH);
     simulateMouseEvent(document, "mouseup", middleW, middleH);
+
+    await wait(200);
+
+    addToTable(
+      first.getAttribute("data-item-text"),
+      second.getAttribute("data-item-text")
+    );
   }, 100);
 }
-
 
 function erase() {
   const el = document.elementFromPoint(middleW, middleH);
@@ -90,8 +123,6 @@ function erase() {
 
 controller();
 
-
-
 function wait(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
